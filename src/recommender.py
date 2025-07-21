a1c04a4e3b1d915e8f9f2ecd0237bd2011412aba
Tiffany Smith <thirstsnake@MacBookPro.lan>
2025-07-21 16:44:18 -0400

    Update folder structure and CSV files. Add recommender.py

---
+++ src/recommender.py
@@ -0,0 +1,27 @@
+# Define recommend random swag
+def recommend_random_swag(df):
+    item = df.sample(1).iloc[0]
+    return {
+        "name": item["name"],
+        "description": item["description"],
+        "image_path": item["image_path"]
+    }
+
+# Define recommend random beer
+def recommend_random_beer(df):
+    beer = df.sample(1).iloc[0]
+    return {
+        "name": beer["name"],
+        "tagline": beer["tagline"],
+        "abv": beer["abv"],
+	    "origin": beer["origin"]
+    }
+
+#Define recommend random song
+def recommend_random_song(df):
+    item = df.sample(1).iloc[0]
+    return {
+        "title": item["title"],
+        "artist": item["artist"],
+        "spotify_url": item["spotify_url"]
+    }
